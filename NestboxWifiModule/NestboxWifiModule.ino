54bea3b21d6a5cdd96d1b045b99511a23e7a8f93
traffael <tschui@kth.se>
2018-11-28 15:34:52 +0100

    implemented timestamp request to MSP. not working yet on current hardware

--- NestboxWifiModule/NestboxWifiModule.ino
+++ NestboxWifiModule/NestboxWifiModule.ino
@@ -9,8 +9,8 @@
 #include <ESP8266WebServer.h>
 #include <DNSServer.h>
 
-#define DEBUG_SERIAL  Serial
-#define APP_SERIAL  Serial1
+#define DEBUG_SERIAL  Serial1 //todo check
+#define APP_SERIAL  Serial
 
 const byte DNS_PORT = 53;
 IPAddress apIP(192, 168, 1, 1);
@@ -57,20 +57,30 @@ void min_tx_byte(uint8_t port, uint8_t byte)
   while(n == 0);
 }
 
-void write_variable(){
+void write_variable(char code, unsigned int len, uint32_t value){
    //delay(1000);
   //For sending time
   uint8_t tx_buf [32];
-  tx_buf[0] = 'Z' | 0x80;
-  for(int i = 1; i <= 4; i++){
-    tx_buf[i] = 0x33;
+  tx_buf[0] = code | 0x80;
+  for(int i = 0; i < len; i++){
+    tx_buf[len-i] = value & 0xFF;
+    value = value >> 8;
   }
    min_send_frame(&min_ctx, 0x33, tx_buf, 0x05);
 }
 
-void read_variable(){
+void request_variable(char code){
+   //delay(1000);
+  //For sending time
+  uint8_t tx_buf [32];
+  tx_buf[0] = code;
+  min_send_frame(&min_ctx, 0x33, tx_buf, 1);
+}
+
+uint32_t read_variable(char* code){
   uint8_t rx_buf [32];
   size_t buf_len;
+  uint32_t result = 0;
   if(Serial.available() > 0) {
     buf_len = Serial.readBytes(rx_buf, 32U);
   }
@@ -80,13 +90,16 @@ void read_variable(){
  min_poll(&min_ctx, rx_buf, (uint8_t)buf_len);
 
  Serial1.printf("Recieved message %c \n", min_ctx.rx_frame_payload_buf[0]);
- Serial1.printf("%d", min_ctx.rx_frame_payload_buf[1]);
- Serial1.printf("%d", min_ctx.rx_frame_payload_buf[2]);
- Serial1.printf("%d", min_ctx.rx_frame_payload_buf[3]);
- Serial1.printf("%d", min_ctx.rx_frame_payload_buf[4]);
- Serial1.printf("\n");
+ *code = min_ctx.rx_frame_payload_buf[0];
 
-
+ for(int i=1; i<min_ctx.rx_frame_payload_bytes; i++)
+ {
+   Serial1.printf("%d", min_ctx.rx_frame_payload_buf[i]);
+   result = result<<8 + min_ctx.rx_frame_payload_buf[i];
+ }
+ 
+   Serial1.printf("\n");
+   return result;
 }
 
 
@@ -108,10 +121,29 @@ void min_application_handler(uint8_t min_id, uint8_t *min_payload, uint8_t len_p
 //The functions for the DNS server
 
 void timeUpdate(){
-  String t = "123424453";
-  server.send(200, "text/plain", t);
+  uint32_t value = 1234;
+  char code;
+  write_variable('Z', 4, value);
+  delay(100);
+  value = read_variable(&code);
+  String t = String(value);
+  server.send(200, "text/plain", code+t);
 }
 
+void batteryUpdate(){
+  uint32_t value = 0;
+  char code;
+  request_variable('B');
+  delay(100);
+  value = read_variable(&code);
+  String t = String(value);
+  server.send(200, "text/plain", code+t);
+}
+
+void HelloWorld(){
+  String t = "Hello World";
+  server.send(200, "text/plain", t);
+}
 
 void handleUpdateTable(){
   if( ! server.hasArg("cur-time") || server.arg("cur-time")) { // If the POST request doesn't have username and password data
@@ -155,21 +187,7 @@ unsigned long uid_mem[TABLE_LENGTH];
 unsigned long time_mem[TABLE_LENGTH];
 short in_out_mem[TABLE_LENGTH];
 
-void update_passages()
-{ 
-  APP_SERIAL.setTimeout(2000);
-  byte uid_input[MSG_LENGTH];
 
-  byte readlength = APP_SERIAL.readBytes(uid_input,MSG_LENGTH);
-  if(readlength == MSG_LENGTH)
-  {
-    uid_mem[buffer_pos] = *(unsigned long*)(&uid_input[UID_POS]);    //0xdeadbeef;
-    time_mem[buffer_pos] = *(unsigned long*)(&uid_input[TIMESTAMP_POS]); //1000 + 10 * buffer_pos;
-    in_out_mem[buffer_pos] = uid_input[INOUT_POS];
-    buffer_pos += 1;
-    buffer_pos = buffer_pos % TABLE_LENGTH;
-  }
-}
 void setup() {
   Serial.begin(9600);
   Serial1.begin(9600);
@@ -184,11 +202,6 @@ void setup() {
 
 
   //Begin the wifi and DNS server
-  APP_SERIAL.begin(115200);
-  APP_SERIAL.swap();
-  
-  DEBUG_SERIAL.begin ( 115200 );
-
   WiFi.mode(WIFI_AP);
   WiFi.softAPConfig(apIP, apIP, IPAddress(255, 255, 255, 0));
   WiFi.softAP("Nestbox123");
@@ -199,7 +212,10 @@ void setup() {
   // start DNS server for a specific domain name
   dnsServer.start(DNS_PORT, "www.nestbox.local", apIP);
 
-  server.on ("/z",   timeUpdate);
+  server.on ("/time", timeUpdate);
+  server.on ("/battery", batteryUpdate);
+  server.on ("/Hello_World", HelloWorld);
+
   server.on("/", handleRoot);
   server.on("/time", HTTP_POST, handleUpdateTable);
   server.onNotFound (handleNotFound);
